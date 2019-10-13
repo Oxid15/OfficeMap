df060a4453247265ee5611a9f69967724832b0d6
SofyaM777 <55793215+SofyaM777@users.noreply.github.com>
2019-10-13 12:48:40 +0500

    Add files via upload

---
+++ структура.cs
@@ -0,0 +1,50 @@
+struct User
+{
+    int userId;
+    string login;
+    string lastName;
+    string firstName;
+    string middleName;
+    string picture;
+    boolean isAdmin;
+    string email;  
+}
+    
+struct Map 
+{
+    string url;
+
+}
+struct Location
+{
+    string placeId;
+    (int||null) userId;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+}
+
+
+    User addUser(string login, string lastName,
+    string firstName, string middleName, string picture, string email) 
+    {
+        
+    }         
+
+    User updateUser(int userId)
+    {}
+
+    User getUser(int userId){}
+
+    User deleteUser(int userId){} 
+
+    User[] getUsers(){}
+
+    Location getLocationByUser(int userId){}
+
+    Location[] getLocations(){}
+
+    User[] findUsers(string query){}
+
+    Location addLocation(string placeId){}
+
+    Location deleteUser(string placeId){}
+
+    string getMapUrl(){}
